9e2ef9300ab938be665383edfbbd0f5c8bf27c98
Nicholas Schuetze <Nicholas Schuetze>
2015-11-29 23:26:03 -0600

    Added to createListenSocket

--- RouterClient.py
+++ RouterClient.py
@@ -1,30 +1,42 @@
 from socket import *
+import threading
 
 class Node:
     nodeIP = ''
     nodeID = 0
 
-def listenSocket(port):
+#Opens a socket to listen other nodes
+def createListenSocket(port):
     global nodes
-    serverSocket = socket(AF_INET, SOCK_STREAM)
+    listenSocket = socket(AF_INET, SOCK_STREAM)
     print 'Socket Created'
-    serverSocket.bind(('', port))
-    serverSocket.listen(5)
+    listenSocket.bind(('', port))
+    listenSocket.listen(5)
     print 'Ready to serve...'
     while True:
         # Establish the connection
-        connectionSocket, addr = serverSocket.accept()
+        connectionSocket, addr = listenSocket.accept()
+        try:
+            message = connectionSocket.recv(1024) #size may need to be adjusted when format of the packet format has be finalized
+            print message
+            #Need to parse message and store information
+            #Two possible types of messages, routing table updates from other nodes, node address information from server
 
-
+#Connects to the server to be added to the list of nodes
 def connectToServer(TCP_IP,TCP_PORT):
     sock = socket(AF_INET,SOCK_STREAM)
     sock.connect((TCP_IP,TCP_PORT))
 
+
+def sendToNode():
+
+
 def main():
     TCP_IP = '76.183.92.14'
     TCP_PORT = 8007
+    serverConnectThread = threading.Thread(target=connectToServer(TCP_IP,TCP_PORT))
+    serverConnectThread.start()
 
-    connectToServer(TCP_IP,TCP_PORT)
     while True:
         pass
 
