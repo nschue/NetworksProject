23f1a1a153945e382f57bde10184d54bf7f39075
Zelmec <kinlerj@gmail.com>
2015-12-02 01:12:41 -0600

    Cleaned and commented server file, added send functions
    I haven't gotten to test it yet, but these additions to the server code
    should make its function more clear.
    
    I also added some code at the bottom of listenSocket() that should have
    the server send all nodes back to a client after it connects.

--- ServerRouter.py
+++ ServerRouter.py
@@ -15,26 +15,46 @@ class Node:
 
 def listenSocket(port):
     global nodes
+	
+	# TCP/IP socket created
     serverSocket = socket(AF_INET, SOCK_STREAM)
     print 'Socket Created'
-    serverSocket.bind(('', port))
-    serverSocket.listen(5)
-    print 'Ready to serve...'
-    while True:
+	
+	# The socket gets bound to the port
+    serverSocket.bind(('localhost', port))
+    serverSocket.listen(5) # Listen for connections
+    print 'Listening for connections...'
+    
+	while True:
         # Establish the connection
-        connectionSocket, addr = serverSocket.accept()
+        connectionSocket, client_IP = serverSocket.accept()
+		print 'Connection made'
+		
+		# Create a temporary node to hold the IP address of the latest connection
         tempNode = Node()
-        tempNode.nodeIP = addr[0]
+        tempNode.nodeIP = client_IP[0]
         tempNode.nodeID = len(nodes)
+		
+		#Check for duplicate nodes
 		for x in nodes:
-			if(tempNode.nodeIP = x.nodeIP)
+			if(tempNode.nodeIP = x.nodeIP):
 				break
 				
+		# Add any new nodes to the global list of nodes
         nodes.append(tempNode)
         for x in nodes:
-            print 'Node ' + str(x.nodeID) + ': ' + x.nodeIP
+            print 'Node ' + str(x.nodeID) + ': ' + x.nodeIP 
+			
+		try:
+			print 'Connection from', client_IP
 
-
+			# Send a node ID number followed by its corresponding IP to the client
+			for x in nodes: 
+				connectionSocket.sendall(x.nodeID) 
+				connectionSocket.sendall(x.nodeIP)
+				
+		finally:
+			connectionSocket.close()
 
 def main():
     listenThread = threading.Thread(target=listenSocket(8007))
