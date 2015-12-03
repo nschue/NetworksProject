5e7d917b2ca1f3f765b6c9583427174fe20a0a59
Nicholas Schuetze <Nicholas Schuetze>
2015-12-02 23:17:11 -0600

    Client parses information from server and stores node information"

--- RouterClient.py
+++ RouterClient.py
@@ -4,7 +4,7 @@ import threading
 # Global variables
 nodes = []
 neighbors = []
-cost_matrix = [[0 for x in range(20)] for x in range(20)]
+cost_Matrix = [[float('inf') for x in range(20)] for x in range(20)]
 
 
 class Node:
@@ -14,52 +14,74 @@ class Node:
 
 
 # Opens a socket to listen other nodes
-def createListenSocket(port):
+def node_listen_socket(port):
     global nodes
-    listenSocket = socket(AF_INET, SOCK_STREAM)
+    listen_socket = socket(AF_INET, SOCK_STREAM)
     print 'Socket Created'
-    listenSocket.bind(('', port))
-    listenSocket.listen(5)
+    listen_socket.bind(('', port))
+    listen_socket.listen(5)
     print 'Ready to serve...'
     while True:
         # Establish the connection
-        connectionSocket, addr = listenSocket.accept()
+        connectionSocket, addr = listen_socket.accept()
         try:
             message = connectionSocket.recv(1024)
             # size may need to be adjusted when format of the packet has be finalized
             print message
             # Need to parse message and store information
-            # Two possible types of messages, routing table updates from other nodes,
-            # node address information from server
         except:
             print "Error in createListenSocket"
 
+
+def server_listen_socket(port):
+    global nodes
+    listen_socket = socket(AF_INET, SOCK_STREAM)
+    print 'Socket Created'
+    listen_socket.bind(('', port))
+    listen_socket.listen(0)
+    print 'Ready to serve...'
+    # Establish the connection
+    try:
+        connection_socket, addr = listen_socket.accept()
+        message = connection_socket.recv(1024)
+        # size may need to be adjusted when format of the packet has be finalized
+        print message
+        message = message.split(',')
+        for i in range(len(message)/2):
+            temp_node = Node()
+            temp_node.nodeID = int(message[2 * i])
+            temp_node.nodeIP = message[2 * i + 1]
+            nodes.append(temp_node)
+
+        # Need to parse message and store information
+        # node address information from server
+    except:
+        print "Error in server_listen_socket"
 
 # Connects to the server to be added to the list of nodes
 def connectToServer(TCP_IP, TCP_PORT):
     sock = socket(AF_INET, SOCK_STREAM)
     try:
-        sock.connect((TCP_IP,TCP_PORT))
+        sock.connect((TCP_IP, TCP_PORT))
     except:
         print"Error in connectToServer"
 
 
 # Function to call when sending out routing table information
 # Thinking updateNode function will iterate through the nodes list passing each node into this fuction
-def sendToNode(node,TCP_PORT, data):
+def send_to_node(node,TCP_PORT, data):
     sock = socket(AF_INET, SOCK_STREAM)
     try:
         sock.connect((node.nodeIP, TCP_PORT))
         sock.send(data)  # Routing Table will be passed through here
     except:
-        print ("Error in sendToNode")
-    return
+        print ("Error in send_to_node")
 
 
 # iterate through each node in the neighbors list to pass the updated routing information to each node
-def updateNodes(TCP_PORT, data):
+def update_nodes(TCP_PORT, data):
     for node in neighbors:
-        sendToNode(node, TCP_PORT, data)
+        send_to_node(node, TCP_PORT, data)
     return
 
 
@@ -68,11 +90,14 @@ def main():
     TCP_PORT = 8007
     serverConnectThread = threading.Thread(target=connectToServer(TCP_IP,TCP_PORT))
     serverConnectThread.start()
+    server_listen_socket(TCP_PORT)
+    while 1:
+        pass
     # listen for information from server
     # after server node information has been collected
     print 'I am node: '
     neighbor_input = raw_input('Which nodes am I connecting to (nodeID)? \n\'-1\' to signal no more neighbors.')
-    while neighbor_input != '-1':
+    while neighbor_input != '-1':  # Adds nodes to neighbors by reading nodeID from input
         try:
             neighbors.append(nodes[int(neighbor_input)])  # Adds neighbor from the nodes list which is filled by the server
         finally:
@@ -81,6 +106,7 @@ def main():
     for node in neighbors:
         print "Node", node.nodeID
 
+    # need to add distance to neighbors here
     while True:
         pass
 
