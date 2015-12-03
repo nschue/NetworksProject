1d6f4da3433affa155e49e7122e1a8df4add0eed
Nicholas Schuetze <Nicholas Schuetze>
2015-12-03 01:22:09 -0600

    Adapted the DVR into a funtion that returns an updated cost matrix

---
+++ DVR.py
@@ -0,0 +1,40 @@
+class node:
+    def __init__(self, ID=None, dist = None, fro = None):
+        self.ID   = ''
+        self.dist = [0]*20
+        self.fro  = [0]*20
+
+
+def dvr(number_nodes, costmat):
+    rt = [0]*10
+
+    for i in range(10):
+        rt.insert(i, node('ID', 0, 0))
+
+    print 'Number of nodes: ',number_nodes
+    number_nodes=int(number_nodes)
+
+    for i in range(number_nodes):
+        for j in range(number_nodes):
+            costmat[i][i]=0
+            rt[i].dist[j]=costmat[i][j]
+            rt[i].fro[j]=j
+
+    count=0
+    while True:
+        for i in range(number_nodes):
+            for j in range(number_nodes):
+                for k in range(number_nodes):
+                    if rt[i].dist[j]>costmat[i][k]+rt[k].dist[j]:
+                        rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j]
+                        rt[i].fro[j]=k
+                        count+=1
+        if count!=0:
+            break
+
+    for i in range(number_nodes):
+        for j in range(number_nodes):
+            costmat[i][j] = rt[i].dist[j]
+    return costmat
+
+
